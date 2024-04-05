b4009ec43b66fcdc5bcea888fe6d76940f9937d1
Tan Hao Xiang <haoxiang@coingecko.com>
2024-04-05 12:33:10 +0800

    add home page and connect wallet button

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Providers } from "./providers";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -10,8 +11,15 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+      <html>
+        <body className={inter.className }>
+          <div className="mx-4 my-4">
+            <Providers>
+              {children}
+            </Providers>
+          </div>
+          </body>
+          
+      </html>
   );
 }
